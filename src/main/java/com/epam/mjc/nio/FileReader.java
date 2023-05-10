95e96f3b4db52b7ef65901d63788da5b1f36651a
jargr <jaroslav.grablevski@gmail.com>
2023-05-10 19:43:17 +0300

    nio profile

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,43 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+
+        try (Stream<String> lines = Files.lines(file.toPath())) {
+            lines.forEach(line -> {
+                String[] keyValue = line.split(": ");
+
+                switch (keyValue[0]) {
+                    case "Name":
+                        profile.setName(keyValue[1]);
+                        break;
+                    case "Age":
+                        profile.setAge(Integer.parseInt(keyValue[1]));
+                        break;
+                    case "Email":
+                        profile.setEmail(keyValue[1]);
+                        break;
+                    case "Phone":
+                        profile.setPhone(Long.parseLong(keyValue[1]));
+                        break;
+                    default:
+                        break;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return profile;
+
+
     }
 }
